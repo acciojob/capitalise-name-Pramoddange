a595ee7698818f05f50810191009d2d023fa13c3
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-03 01:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
             const inputField = document.getElementById('fname');
 
             // Add an event listener for the 'blur' event
-            inputField.addEventListener('blur', function() {
+            inputField.addEventListener('blur', ()=> {
                 // Convert the content of the input field to uppercase
                 inputField.value = inputField.value.toUpperCase();
             });
